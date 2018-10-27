4865ee5bd6cb6be736a1a87040ab99586e74423b
Robin Kim <rkim1214@gmail.com>
2018-10-27 17:09:41 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,6 +13,18 @@ end
 
 def word_substituter(tweet)
   keys = dictionary.keys
-  
+  values = dictionary.values 
   tweet_array = tweet.split
+  
+  tweet_array.each do |word|
+    count = 0 
+    until count > 9
+      if word == keys[count]
+        word = values[count]
+        count += 1 
+      end 
+    end 
+  end 
+  
+  tweet_array.join(" ")
 end 
