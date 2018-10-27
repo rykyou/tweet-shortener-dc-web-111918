e034b0cc50183dbe91102291a2207629dbbffe50
Robin Kim <rkim1214@gmail.com>
2018-10-27 18:29:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -23,6 +23,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(many_tweets)
-  tweet = tweet.join(' ')
-  puts word_substituter(tweet)
+  many_tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end 
 end 
