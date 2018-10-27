82a4da391b81db3673cfdcf0d1a4a782dca9ebbe
Robin Kim <rkim1214@gmail.com>
2018-10-27 18:45:51 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,7 +38,7 @@ def selective_tweet_shortener(tweet)
 end 
 
 def shortened_tweet_truncator(tweet)
-  if selective_tweet_shortener(tweet) > 140
+  if selective_tweet_shortener(tweet).length > 140
     selective_tweet_shortener(tweet)[0...137] + "..."
   else 
     selective_tweet_shortener(tweet)
