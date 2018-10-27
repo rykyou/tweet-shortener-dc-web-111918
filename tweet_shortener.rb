51310c3f5c29b0351ab5e59c962f0ba35054d1ad
Robin Kim <rkim1214@gmail.com>
2018-10-27 16:58:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,14 @@
 def dictionary
   substitutes = {
-    "hello" => 'hi'
-    "to" => '2' 
-    "two" => '2'
-    "too" => '2'
-    "for" => '4'
-    "four"=> '4'
-    'be' => 'b'
-    'you' => 'u'
-    "at" => "@" 
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four"=> '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
     "and" => "&"
   }
 end 
