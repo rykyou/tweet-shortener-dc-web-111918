b186aac7265e23a2f85851e7d4343ba9d63c2f67
Robin Kim <rkim1214@gmail.com>
2018-10-27 18:46:03 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,46 @@
-# Write your code here.
+def dictionary
+    {"hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "For" => '4',
+    "four"=> '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
+    "and" => "&"}
+end 
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(' ')
+  tweet_array.map do |word|
+    if dictionary[word] 
+      dictionary[word] 
+    else
+      word 
+  	end
+  end.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end 
+end 
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140 
+    word_substituter(tweet)
+  else
+    tweet
+  end 
+end 
+
+def shortened_tweet_truncator(tweet)
+  if selective_tweet_shortener(tweet).length > 140
+    selective_tweet_shortener(tweet)[0...137] + "..."
+  else 
+    selective_tweet_shortener(tweet)
+  end 
+end 
